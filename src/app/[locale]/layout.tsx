b6796232a31ff94207d098834d37781500f4cd04
hikkywannafly <ggraygon@gmail.com>
2025-11-03 11:30:04 +0700

    style: change font style to nunito

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,16 @@
 import { Analytics } from "@vercel/analytics/react";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { Poppins } from "next/font/google";
+import { Nunito } from "next/font/google";
 import { notFound } from "next/navigation";
 import i18nConfig from "../../i18nConfig";
 import "../globals.css";
 
-const poppins = Poppins({
-  weight: ["300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
+const nunito = Nunito({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin", "vietnamese"],
+  variable: "--font-nunito",
+  display: "swap",
 });
 
 type Props = {
@@ -35,7 +36,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
-        className={`${poppins.className} bg-[#F9F6F2] text-[#39241A] antialiased`}
+        className={`${nunito.variable} bg-[#F9F6F2] font-sans text-[#39241A] antialiased`}
       >
         <Analytics />
         <NextIntlClientProvider messages={messages}>
